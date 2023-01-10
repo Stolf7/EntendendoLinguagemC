9b85f774fc2a5439317087227ebe4d31f51e83d6
Matheus Stolf <116499516+Stolf7@users.noreply.github.com>
2023-01-10 13:56:42 -0300

    Update c_aulas.c

--- c_aulas.c
+++ c_aulas.c
@@ -1,4 +1,4 @@
-Aula 1
+// Conceito 1 // incluir bibliotecas
 
 #include <stdio.h>  /* ----->  Serve para fazermos a inclusão da bibilioteca */
 					// standart input output
@@ -12,7 +12,7 @@ int main ()  /* ----->  Para criarmos a funçaõ principal */
 	return 0;
 }
 
-Aula 2 
+// Conceito 2 // funcao main 
 
 #include <stdio.h>
 
@@ -26,7 +26,7 @@ int main ()
 	return 0;
 }
 
-Aula 3
+// Conceito 3 // fazer comentarios em c
 
 #include <stdio.h> //standart input output
 
@@ -43,7 +43,7 @@ int main()
 	return 0;
 }
 
-Aula 4 
+// Conceito 4 // conceito e criacao de variaveis 
 
 #include <stdio.h> 
 
@@ -65,3 +65,91 @@ int main()
 	
 	return 0;
 }
+
+// Conceito 6 // operacoes matematicas
+
+#include <stdio.h>
+
+int main()
+{
+	int minha_idade = 20;
+	int mae_idade = 40;
+	int pai_idade = 45;
+	int irmao_idade = 15;
+	
+	
+	int idade_total = minha_idade + mae_idade + pai_idade + irmao_idade;
+	
+	printf("A soma das idades e: %i\n", idade_total);
+	
+	return 0;
+
+	
+}
+
+// Conceito 7 // scand // obter dados do usuario
+
+
+#include <stdio.h>
+
+int main()
+{
+	
+	int base;
+	int altura;
+	int area;
+	
+	area = base * altura;
+		
+	printf ("Digite o valor da base: ");
+	scanf ("%i", &base);
+	
+	printf ("Digite o valor da altura: ");
+	scanf ("%i", &altura);
+		
+	printf ("O valor da area e = %i", area);
+	
+	return 0;
+	
+}
+
+// Conceito 8 // char, float, int
+
+#include <stdio.h>
+
+int main()
+{
+	int inteira = 10;
+	// int para numeros inteiros
+	
+	float decimal = 10.5;
+	// usamos flot para valores com virgula
+	
+	char caractere = 'a';
+	// char para caracteres
+	
+	printf("inteiro = %i\ndecimal = %f\n caractere = %c\n", inteira, decimal, caractere);
+	
+	return 0;
+}
+
+// Conceito 9 // Contador
+
+#include <stdio.h>
+
+int main()
+{
+	
+	int contador;
+	
+	//estrutura do for
+	//(valor incial, limite, fazer algo )
+	for (contador = 1; contador <= 10; contador = contador + 1)
+	{
+		printf("teste\n");
+	}
+	return 0;
+	
+}
+
+// 
