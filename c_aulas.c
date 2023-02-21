f7ed48d1faf741cbf6680d659066a7a9c2cd7cdf
Matheus Stolf <116499516+Stolf7@users.noreply.github.com>
2023-02-20 22:56:41 -0300

    Update c_aulas.c

--- c_aulas.c
+++ c_aulas.c
@@ -369,4 +369,47 @@ int main()
 	return 0;
 }
 
+// VETORES
+
+#include <stdio.h>
+
+int main(void){
+	
+	int vetor [3] = {1, 2, 3};
+	// primeiro [] -> linha
+	// segunda [] -> coluna
+	int matriz1 [3] [3] = {{1, 2, 3},
+						  {4, 5, 6},
+						  {7, 8, 9}};
+	// ou
+	
+	int matriz2 [3] [3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};	
+	
+	printf("%i", matriz1 [0] [0]);				  
+	
+	
+	return 0;
+}
+
+// REPRESENTANDO MATRIZES
+
+#include <stdio.h>
+
+int main(void){
+	
+	int matriz [5] [5] = {{1,  2,  3,  4,   5},
+							{6, 7,  8,   9,  10},
+							{11, 12, 13, 14, 15}, 
+							{16, 17, 18, 19, 20}, 
+							{21, 22, 23, 24, 25}};
+	
+	for (int m = 0; m < 5; m = m + 1){
+		for (int n = 0; n < 5; n = n + 1){ // o programa roda o ciclo interno ate atingir o criterio
+			printf("%i ", matriz [m] [n]); // ao completa, passara para a segunda linha
+		printf("\n");
+		}
+	}
+} 
+
+// 
 
